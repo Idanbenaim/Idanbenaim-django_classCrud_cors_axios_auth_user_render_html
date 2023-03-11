e9428a7833b02b6a3aab8cb2b6af2ae6c0e4a38a
Idanbenaim <idanbenaim@gmail.com>
2023-03-11 21:26:21 +0200

    added login page and auth

--- base/views.py
+++ base/views.py
@@ -1,13 +1,15 @@
 from .models import Students
+# from django.shortcuts import render
 
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework import serializers
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 # Create your views here.
@@ -20,6 +22,20 @@ class StudentsSerializer(serializers.ModelSerializer):
         model = Students
         fields = '__all__'
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+        return token
+ 
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+# @api_view(['GET'])
+@permission_classes([IsAuthenticated])
 class MyStudentsView(APIView):
     def get(self, request, id=-1):  # axios.get
         if int(id) > -1:
